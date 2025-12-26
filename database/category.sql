dff9ba1a41c0d3a3d1c6eb8e2c60a8dd2679d800
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 10:31:39 +0530

    commited by n8n

---
+++ database/category.sql
@@ -0,0 +1,6 @@
+CREATE TABLE category (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    parent_id INTEGER,
+    FOREIGN KEY (parent_id) REFERENCES category (id) ON DELETE CASCADE
+);
