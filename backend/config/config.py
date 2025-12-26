9044cac4a44d24057ccbac0eae8ea804eeaa9b78
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 10:20:33 +0530

    commited by n8n

--- backend/config/config.py
+++ backend/config/config.py
@@ -6,4 +6,4 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_TIMEOUT = int(os.environ.get("SESSION_TIMEOUT", 3600))
     MAX_LOGIN_ATTEMPTS = int(os.environ.get("MAX_LOGIN_ATTEMPTS", 5))
-    RESET_TOKEN_EXPIRY = int(os.environ.get("RESET_TOKEN_EXPIRY", 3600))
+    RESET_TOKEN_EXPIRY = int(os.environ.get("RESET_TOKEN_EXPIRY", 86400))  # 24 hours
