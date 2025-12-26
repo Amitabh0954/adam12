6d463d3c18c2684198e170ed650b44c22cdcf292
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 10:50:02 +0530

    commited by n8n

--- backend/database/category.sql
+++ backend/database/category.sql
@@ -1,5 +1,7 @@
 CREATE TABLE category (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL UNIQUE,
-    description TEXT
+    description TEXT,
+    parent_id INTEGER,
+    FOREIGN KEY (parent_id) REFERENCES category (id) ON DELETE SET NULL
 );
