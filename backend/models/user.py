892424412f9c2f557e3b70d847d839a8cebc3e1b
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 10:48:01 +0530

    commited by n8n

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,5 +1,7 @@
 class User:
-    def __init__(self, id: int, email: str, password: str):
+    def __init__(self, id: int, email: str, password: str, login_attempts: int = 0, is_locked: bool = False):
         self.id = id
         self.email = email
         self.password = password
+        self.login_attempts = login_attempts
+        self.is_locked = is_locked
