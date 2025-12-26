5aae8da37e869e1a047f8ae5c1d2383eba6ac68e
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 10:07:39 +0530

    commited by n8n

--- backend/config/config.py
+++ backend/config/config.py
@@ -4,3 +4,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'supersecretkey')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SESSION_TIMEOUT = int(os.environ.get("SESSION_TIMEOUT", 3600))
