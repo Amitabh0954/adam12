654bfd4eb5bdd22efacdb99666db74ad98a91d8a
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-16 19:47:14 +0530

    commited by n8n

--- database/create_user_table.sql
+++ database/create_user_table.sql
@@ -23,34 +23,11 @@ CREATE TABLE profile (
     FOREIGN KEY(user_id) REFERENCES user(id)
 );
 
-CREATE TABLE category (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL UNIQUE,
-    parent_id INTEGER,
-    FOREIGN KEY(parent_id) REFERENCES category(id)
-);
-
 CREATE TABLE product (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL UNIQUE,
     price REAL NOT NULL,
     description TEXT NOT NULL,
-    category_id INTEGER NOT NULL,
-    is_deleted BOOLEAN NOT NULL DEFAULT 0,
-    FOREIGN KEY(category_id) REFERENCES category(id)
-);
-
-CREATE TABLE cart (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER,
-    FOREIGN KEY(user_id) REFERENCES user(id)
-);
-
-CREATE TABLE cart_item (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    cart_id INTEGER NOT NULL,
-    product_id INTEGER NOT NULL,
-    quantity INTEGER NOT NULL,
-    FOREIGN KEY(cart_id) REFERENCES cart(id),
-    FOREIGN KEY(product_id) REFERENCES product(id)
+    category TEXT,
+    is_deleted BOOLEAN NOT NULL DEFAULT 0
 );
