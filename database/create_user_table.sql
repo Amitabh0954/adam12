12cda493acd4fb1e894c1576ea124e949f73aaa0
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-16 19:46:16 +0530

    commited by n8n

--- database/create_user_table.sql
+++ database/create_user_table.sql
@@ -39,3 +39,18 @@ CREATE TABLE product (
     is_deleted BOOLEAN NOT NULL DEFAULT 0,
     FOREIGN KEY(category_id) REFERENCES category(id)
 );
+
+CREATE TABLE cart (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    FOREIGN KEY(user_id) REFERENCES user(id)
+);
+
+CREATE TABLE cart_item (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cart_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
+    quantity INTEGER NOT NULL,
+    FOREIGN KEY(cart_id) REFERENCES cart(id),
+    FOREIGN KEY(product_id) REFERENCES product(id)
+);
