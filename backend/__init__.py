fa76cc6dbfcc955ded9c56dc91057d31b8e835a4
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-16 19:43:27 +0530

    commited by n8n

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_session import Session
 from backend.account.models import db
 from backend.account.views import account_bp
 
@@ -7,9 +8,13 @@ def create_app() -> Flask:
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.config['SECRET_KEY'] = 'supersecretkey'
+    app.config['PERMANENT_SESSION_LIFETIME'] = 1800  # Session timeout 30 minutes.
 
     db.init_app(app)
     Migrate(app, db)
+    Session(app)
 
     app.register_blueprint(account_bp, url_prefix='/account')
 
