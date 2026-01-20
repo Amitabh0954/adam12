4df8b4675fc15b427ad0a80e71a21dd7876e6cd0
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-20 15:22:48 +0530

    Update backend/profile/__init__.py

--- backend/profile/__init__.py
+++ backend/profile/__init__.py
@@ -4,9 +4,7 @@ from .models import db
 from .routes import profile_bp
 
 def init_app(app):
-    # Register blueprint
     app.register_blueprint(profile_bp, url_prefix='/profile')
 
-    # Create tables
     with app.app_context():
         db.create_all()
