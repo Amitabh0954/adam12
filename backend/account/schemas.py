8cb620000872cbdbc968d33f9fa2aa52caf05897
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-20 15:17:35 +0530

    Update backend/account/schemas.py

--- backend/account/schemas.py
+++ backend/account/schemas.py
@@ -1,10 +1,10 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
 
 class UserSchema(Schema):
     id = fields.Int(dump_only=True)
-    username = fields.Str(required=True)
-    email = fields.Email(required=True)
-    password = fields.Str(required=True, load_only=True)
-
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
+    email = fields.Email(required=True, validate=validate.Length(max=120))
+    password = fields.Str(load_only=True, required=True, validate=[validate.Length(min=8), validate.Regexp(
+        regex='(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)',
+        error='Password must contain at least one uppercase letter, one lowercase letter, and one number'
+    )])
+    created_at = fields.DateTime(dump_only=True)
