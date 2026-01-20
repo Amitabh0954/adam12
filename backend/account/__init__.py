ea612dbbf165731fc529599bd8c6fd43dd653cb0
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-20 15:22:19 +0530

    Update backend/account/__init__.py

--- backend/account/__init__.py
+++ backend/account/__init__.py
@@ -3,6 +3,7 @@ from flask_session import Session
 from .models import db
 from .routes import account_bp
 from flask_mail import Mail
+from flask_jwt_extended import JWTManager
 
 mail = Mail()
 
@@ -18,10 +19,12 @@ def create_app():
     app.config['MAIL_USE_SSL'] = False
     app.config['MAIL_USERNAME'] = 'your-email@example.com'
     app.config['MAIL_PASSWORD'] = 'your-email-password'
-    
+    app.config['JWT_SECRET_KEY'] = 'super-secret-key'
+
     db.init_app(app)
     Session(app)
     mail.init_app(app)
+    jwt = JWTManager(app)
     
     app.register_blueprint(account_bp, url_prefix='/account')
     
