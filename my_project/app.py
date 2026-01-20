db2fd1e236303d88f78350f8944fb7d7a1ed6b19
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-20 10:18:45 +0530

    commited by n8n

--- my_project/app.py
+++ my_project/app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, session
 from backend.models import db
 from backend.controllers import user_controller
+from flask_session import Session
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['PERMANENT_SESSION_LIFETIME'] = 1800  # 30 minutes
 
 db.init_app(app)
+Session(app)
 app.register_blueprint(user_controller.bp)
 
 if __name__ == '__main__':
