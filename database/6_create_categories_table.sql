0084eda9904be36f21b41f06a978238e526905a4
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-20 15:28:31 +0530

    Update database/6_create_categories_table.sql

--- database/6_create_categories_table.sql
+++ database/6_create_categories_table.sql
@@ -1,6 +1,8 @@
 CREATE TABLE categories (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(100) NOT NULL,
+    parent_id INTEGER,
     product_id INTEGER NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES products (id)
+    FOREIGN KEY (product_id) REFERENCES products (id),
+    FOREIGN KEY (parent_id) REFERENCES categories (id)
 );
