79ba9c4914d0781366c847a0688d580160633292
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-25 18:49:17 +0530

    commited by n8n

---
+++ backend/user_account_management/services/email_service.py
@@ -0,0 +1,23 @@
+import smtplib
+from email.mime.text import MIMEText
+
+class EmailService:
+    def __init__(self, smtp_server: str, smtp_port: int, username: str, password: str):
+        self.smtp_server = smtp_server
+        self.smtp_port = smtp_port
+        self.username = username
+        self.password = password
+
+    def send_email(self, recipient: str, subject: str, body: str):
+        msg = MIMEText(body)
+        msg['Subject'] = subject
+        msg['From'] = self.username
+        msg['To'] = recipient
+
+        with smtplib.SMTP(self.smtp_server, self.smtp_port) as server:
+            server.login(self.username, self.password)
+            server.sendmail(self.username, recipient, msg.as_string())
+
+#### 3. Implement service to handle password reset logic
+
+##### PasswordResetService
