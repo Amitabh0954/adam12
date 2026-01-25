bcd530a1f4e31276d93abe38b802d5ab196b5489
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-25 18:25:50 +0530

    commited by n8n

---
+++ backend/shopping_cart_functionality/services/shopping_cart_service.py
@@ -0,0 +1,37 @@
+from sqlalchemy.orm import Session
+from backend.shopping_cart_functionality.models.shopping_cart import ShoppingCart
+from backend.shopping_cart_functionality.models.shopping_cart_item import ShoppingCartItem
+from backend.shopping_cart_functionality.schemas.shopping_cart_schema import ShoppingCartSchema, ShoppingCartItemSchema
+from marshmallow import ValidationError
+
+class ShoppingCartService:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def get_cart(self, user_id: int) -> ShoppingCart:
+        cart = self.session.query(ShoppingCart).filter_by(user_id=user_id).first()
+        if not cart:
+            cart = ShoppingCart(user_id=user_id)
+            self.session.add(cart)
+            self.session.commit()
+        return cart
+
+    def add_item_to_cart(self, user_id: int, data: dict) -> ShoppingCart:
+        cart = self.get_cart(user_id)
+        try:
+            item_data = ShoppingCartItemSchema().load(data)
+        except ValidationError as err:
+            raise ValueError(f"Invalid data: {err.messages}")
+
+        item = self.session.query(ShoppingCartItem).filter_by(cart_id=cart.id, product_id=item_data['product_id']).first()
+        if item:
+            item.quantity += item_data['quantity']
+        else:
+            item = ShoppingCartItem(cart_id=cart.id, **item_data)
+            self.session.add(item)
+
+        self.session.commit()
+        self.session.refresh(cart)
+        return cart
+
+#### 4. Implement shopping cart controllers
