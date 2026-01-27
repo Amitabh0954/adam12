90ed9f32be9bd7f5acccbae5336049b121fa892c
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 18:50:42 +0530

    commited by n8n

--- 64d2428db0e44eb8a3b7f7733ef2209b-folder-structure/backend/models/product/category.py
+++ 64d2428db0e44eb8a3b7f7733ef2209b-folder-structure/backend/models/product/category.py
@@ -3,10 +3,12 @@
 class Category:
     category_id: int
     name: str
+    parent_id: int
 
-    def __init__(self, category_id: int, name: str):
+    def __init__(self, category_id: int, name: str, parent_id: int = None):
         self.category_id = category_id
         self.name = name
-
+        self.parent_id = parent_id
+    
     def update_category(self, name: str):
         self.name = name
