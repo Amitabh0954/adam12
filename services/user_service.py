9daf379a49e8e2c7e250e04a89deb81696c7fc5f
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-29 11:32:12 +0530

    Adding the implementation for user service

--- services/user_service.py
+++ services/user_service.py
@@ -1,21 +1,19 @@
-from models.user import User
-from schemas.user import UserSchema
 from sqlalchemy.orm import Session
-from marshmallow import ValidationError
+from models.user import User
 
 class UserService:
     def __init__(self, session: Session):
         self.session = session
 
-    def create_user(self, data: dict) -> User:
-        try:
-            user_data = UserSchema().load(data)
-        except ValidationError as err:
-            raise ValueError(f"Invalid data: {err.messages}")
+    def create_user(self, data):
+        email = data.get('email')
+        password = data.get('password')
 
-        new_user = User(**user_data)
-        self.session.add(new_user)
+        if not email or not password:
+            raise ValueError('Email and password are required')
+
+        user = User(email=email)
+        user.set_password(password)
+        self.session.add(user)
         self.session.commit()
-        return new_user
-
-#### 4. Create `controllers/user_controller.py` for Flask endpoints
+        return user
